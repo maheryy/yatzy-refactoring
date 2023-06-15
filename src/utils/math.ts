cc7d78266fb7e8f06cd99373deabd0e6095524f2
Prado <mahery.rsh@gmail.com>
2023-06-16 00:01:22 +0200

    feat: add util functions for occurences calculation

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,3 +2,9 @@ export const sum = (...args: number[]): number => args.reduce((a, b) => a + b, 0
 
 export const sumOf = (value: number, numbers: number[]): number =>
   sum(...numbers.filter((n) => n === value));
+
+export const countOccurrence = (value: number, numbers: number[]): number =>
+  numbers.filter((n) => n === value).length;
+
+export const findFirstOccurrenceAbove = (minOccurrence: number, numbers: number[]): number =>
+  numbers.find((n) => countOccurrence(n, numbers) >= minOccurrence) || 0;
--- test/math.test.ts
+++ test/math.test.ts
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { sum, sumOf } from '../src/utils/math';
+import { countOccurrence, findFirstOccurrenceAbove, sum, sumOf } from '../src/utils/math';
 
 describe('sum function', () => {
   it('should sum up a given set of number', () => {
@@ -26,6 +26,30 @@ describe('sumOf function', () => {
 
   it('should return 0 when having an empty set of number', () => {
     const res = sumOf(1, []);
+    assert.strictEqual(res, 0);
+  });
+});
+
+describe('countOccurrence function', () => {
+  it('should count the occurrence of a specific number in a list', () => {
+    const res = countOccurrence(2, [2, 2, 4, 5, 2]);
+    assert.strictEqual(res, 3);
+  });
+
+  it('should return 0 when no occurrence found for the given number', () => {
+    const res = countOccurrence(1, [2, 2, 4, 5, 2]);
+    assert.strictEqual(res, 0);
+  });
+});
+
+describe('findFirstOccurrenceAbove function', () => {
+  it('should return the first number that occurs above a given condition', () => {
+    const res = findFirstOccurrenceAbove(3, [2, 2, 4, 5, 2]);
+    assert.strictEqual(res, 2);
+  });
+
+  it('should return 0 when no number occurs more than the given condition', () => {
+    const res = findFirstOccurrenceAbove(3, [1, 2, 4, 5, 2]);
     assert.strictEqual(res, 0);
   });
 });
