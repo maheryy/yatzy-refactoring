1b544f6cd6cc593aca0452e5375960317bd4d581
Prado <mahery.rsh@gmail.com>
2023-06-15 23:49:09 +0200

    feat: add util function for specific sums

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,1 +1,4 @@
 export const sum = (...args: number[]): number => args.reduce((a, b) => a + b, 0);
+
+export const sumOf = (value: number, numbers: number[]): number =>
+  sum(...numbers.filter((n) => n === value));
--- test/math.test.ts
+++ test/math.test.ts
@@ -1,14 +1,31 @@
 import assert from 'assert';
-import { sum } from '../src/utils/math';
+import { sum, sumOf } from '../src/utils/math';
 
 describe('sum function', () => {
   it('should sum up a given set of number', () => {
     const res = sum(2, 3, 4, 5, 1);
     assert.strictEqual(res, 15);
   });
-  
+
   it('should return 0 when giving no argument', () => {
     const res = sum();
+    assert.strictEqual(res, 0);
+  });
+});
+
+describe('sumOf function', () => {
+  it('should sum up a matching set of number', () => {
+    const res = sumOf(2, [2, 2, 4, 5, 2]);
+    assert.strictEqual(res, 6);
+  });
+
+  it('should return 0 when no matching number is found in the given set', () => {
+    const res = sumOf(1, [2, 2, 4, 5, 2]);
+    assert.strictEqual(res, 0);
+  });
+
+  it('should return 0 when having an empty set of number', () => {
+    const res = sumOf(1, []);
     assert.strictEqual(res, 0);
   });
 });
